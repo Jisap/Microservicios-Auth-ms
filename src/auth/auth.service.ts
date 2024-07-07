9c9b3ad98e33b118a1855053ffff4a90cf00d564
Jorge Matos <jisapjisap@gmail.com>
2024-07-07 19:06:12 +0200

    loginUser

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { PrismaClient } from '@prisma/client';
-import { RegisterUserDto } from './dto';
+import { LoginUserDto, RegisterUserDto } from './dto';
 import * as bcrypt from 'bcrypt'
+
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit{
@@ -48,6 +49,47 @@ export class AuthService extends PrismaClient implements OnModuleInit{
       }
 
       
+    } catch (error) {
+      throw new RpcException({
+        status: 400,
+        message: error.message
+      })
+    }
+  }
+
+  async loginUser(loginUserDto: LoginUserDto) {
+    
+    try {
+
+      const { email, password } = loginUserDto;
+
+      const user = await this.user.findUnique({
+        where: { email }
+      })
+
+      if (!user) {
+        throw new RpcException({
+          status: 400,
+          message: 'User/Password not valid'
+        })
+      }
+
+      const isPasswordValid = bcrypt.compareSync(password, user.password)
+
+      if(!isPasswordValid){
+        throw new RpcException({
+          status: 400,
+          message: "User/Password not valid"
+        })
+      }
+
+      const { password: __, ...rest} = user;
+
+      return {
+        user: rest,
+        token: 'ABC'
+      }
+
     } catch (error) {
       throw new RpcException({
         status: 400,
